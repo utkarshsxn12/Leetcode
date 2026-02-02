01695765d61d13673cefd0ff457079e69522e858
utkarshsxn12 <153208857+utkarshsxn12@users.noreply.github.com>
2026-02-02 10:43:55 +0530

    Time: 0 ms (100%), Space: 9 MB (67.14%) - LeetHub

--- 3813-vowel-consonant-score/3813-vowel-consonant-score.cpp
+++ 3813-vowel-consonant-score/3813-vowel-consonant-score.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
     int vowelConsonantScore(string s) {
-      int vowel = 0;
-      int consonant = 0;
-      for(char ch: s){
-        if(ch>='a' && ch<='z'){
-            if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
-                vowel++;
-            }
-            else{
-                consonant++;
+        int vowel = 0;
+        int consonant = 0;
+        for(char ch : s){
+            if(ch>='a' && ch<='z'){
+                if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
+                    vowel++;
+                }
+                else{
+                    consonant++;
+                }
             }
         }
-      }
-      if(consonant==0)return 0;
-      return vowel/consonant;
+        if(consonant == 0){
+            return 0;
+        }
+        return vowel/consonant;
     }
 };
