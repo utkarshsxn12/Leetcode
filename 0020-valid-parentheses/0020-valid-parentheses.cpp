c3be7a4f2d004c147d919b0103a25289c308f85e
Utkarsh Saxena <153208857+utkarshsxn12@users.noreply.github.com>
2026-02-10 10:55:49 +0530

    Time: 0 ms (100%), Space: 8.9 MB (37.94%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isValid(string s) {
+      stack<char>st;
+    for(int i=0;i<s.size();i++){
+        if(s[i]=='(' || s[i]=='{' || s[i]=='['){
+            st.push(s[i]);
+
+        }
+        else{
+            if(st.empty()) return false;
+            if(st.top()=='(' && s[i]!=')') return false;
+            if(st.top()=='{' && s[i]!='}') return false;
+            if(st.top()=='[' && s[i]!=']') return false;
+            st.pop();
+        }
+    }
+    if(st.empty()) return true;
+    else return false;
+    }
+};
